1cdab914c6568fd734aec9d2bdd509c5ee898f09
Aleksey <aeksei94@gmail.com>
2022-10-11 20:15:22 +0300

    Лабораторная 1

---
+++ Лабораторная работа 1/task_1.py
@@ -0,0 +1,9 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+unique_numbers = set(list_)
+
+sum_ = sum(unique_numbers)
+len_ = len(unique_numbers)
+
+print(sum_)
+print(len_)
+print(round(sum_ / len_, 5))
