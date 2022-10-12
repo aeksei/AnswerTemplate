3c22948ba03a8475c391aba33ade4c063df2056f
Aleksey <aeksei94@gmail.com>
2022-10-12 09:16:23 +0300

    Delete Лабораторная работа 1 directory

--- Лабораторная работа 1/task_5.py
+++
@@ -1,4 +0,0 @@
-list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-list_numbers[4] = sum(list_numbers) / len(list_numbers)
-print(list_numbers)
