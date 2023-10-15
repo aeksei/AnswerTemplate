21ce057771b3dae95d01894111ba3cd3a6cdc3d2
slonimon <147990126+slonimon@users.noreply.github.com>
2023-10-15 17:07:44 +0300

    lab1_2_3_4

---
+++ lab3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+mid = int(len(list_players) / 2)
+
+print(list_players[:mid])
+print(list_players[mid:])
